009c6fe216270fa4c81db13e3b4cbe66ca11cb5c
James Levine <jamesalexanderlevine@gmail.com>
2019-03-02 12:01:43 +0000

    bootstrap blog and node blog post uplaoding

---
+++ database/models/Post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+
+/// Users, Posts, Products
+
+const PostSchema = new mongoose.Schema({
+  title: String,
+  subtitle: String,
+  content: String,
+  username: String,
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
+})
+
+const Post = mongoose.model('Post', PostSchema)
+
+module.exports = Post

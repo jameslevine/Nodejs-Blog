693b4e9f10707813327f65446126cceccefb4f9a
James Levine <jamesalexanderlevine@gmail.com>
2019-03-05 22:07:46 +0000

    changed user

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const logoutController = require('./controllers/logout')
 
 const app = new express()
 
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds135335.mlab.com:35335/node-js-blog')
+mongoose.connect('mongodb://jameslevine:panther1@ds135335.mlab.com:35335/node-js-blog')
 
 app.use(connectFlash())
 
